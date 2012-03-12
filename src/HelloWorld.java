c40bef01b0470ed13e7642aabfdad2af7882ee94
gyeongjoo <gyeongjoo.cha@amail.co.kr>
2012-03-12 13:23:14 +0900

    second ~!

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -7,6 +7,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("HelloWorld");
+		System.out.println("sencond");
 	}
 
 }
